bb8d10b1f4586eaa1b3f50f34c747fd616aa373a
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-11 14:28:50 +0100

    grupo a añadido

--- app.js
+++ app.js
@@ -13,7 +13,41 @@ app.use(express.urlencoded({ extended: true }));
 // Rutas
 app.get("/", (req, res) => {
   res.json({
-    message: "Hello World",
+    message: "Bienvenido a la api del mundial",
+  });
+});
+
+app.get("/api/nationalteams", (req, res) => {
+  res.json({
+    nationalteams: [
+      {
+        id: 1,
+        nombre: "Catar",
+        grupo: "A",
+        bandera: "https://cdn-icons-png.flaticon.com/512/197/197618.png",
+      },
+      {
+        id: 2,
+        nombre: "Ecuador",
+        grupo: "A",
+        bandera:
+          "https://static.vecteezy.com/system/resources/previews/011/571/449/original/circle-flag-of-ecuador-free-png.png",
+      },
+      {
+        id: 3,
+        nombre: "Senegal",
+        grupo: "A",
+        bandera:
+          "https://cdn.countryflags.com/thumbs/senegal/flag-round-250.png",
+      },
+      {
+        id: 4,
+        nombre: "Paises Bajos",
+        grupo: "A",
+        bandera:
+          "https://static.vecteezy.com/system/resources/previews/011/571/464/non_2x/circle-flag-of-netherlands-free-png.png",
+      },
+    ],
   });
 });
 
