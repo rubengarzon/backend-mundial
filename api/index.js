6ba0df368e1176e40644f0203759a197a03ef262
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-11 13:29:07 +0100

    corregido fallo vercel

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
-import path from "path";
 
 const app = express();
 
@@ -10,17 +9,13 @@ app.use(morgan("tiny"));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(express.static(path.join(__dirname, 'public')));
 
 // Rutas
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({
+    message: "Hello World",
+  });
 });
-
-// Middleware para Vue.js router modo history
-const history = require("connect-history-api-fallback");
-app.use(history());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.set("puerto", process.env.PORT || 3000);
 app.listen(app.get("puerto"), () => {
