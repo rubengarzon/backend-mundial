89c10d7a2c749686ce79184ddc6a0d4331bb406d
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-17 14:31:55 +0100

    corregida bandera gales

--- src/controllers/matches.controller.js
+++ src/controllers/matches.controller.js
@@ -10,9 +10,9 @@ controller.matches = (req, res) => {
         hora: "17:00",
         grupo: "A",
         eliminatoria: "Fase de grupos",
-        banderaCatar:
+        banderaLocal:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Qatar.svg/1200px-Flag_of_Qatar.svg.png",
-        banderaEcuador:
+        banderaVisitante:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1200px-Flag_of_Ecuador.svg.png",
       },
       {
@@ -22,9 +22,9 @@ controller.matches = (req, res) => {
         hora: "14:00",
         grupo: "B",
         eliminatoria: "Fase de grupos",
-        banderaInglaterra:
+        banderaLocal:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Flag_of_England.svg/1200px-Flag_of_England.svg.png",
-        banderaIran:
+        banderaVisitante:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/1200px-Flag_of_Iran.svg.png",
       },
       {
@@ -34,9 +34,9 @@ controller.matches = (req, res) => {
         hora: "17:00",
         grupo: "B",
         eliminatoria: "Fase de grupos",
-        banderaSenegal:
+        banderaLocal:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/1200px-Flag_of_Senegal.svg.png",
-        banderaPaisesBajos:
+        banderaVisitante:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1200px-Flag_of_the_Netherlands.svg.png",
       },
       {
@@ -46,9 +46,9 @@ controller.matches = (req, res) => {
         hora: "20:00",
         grupo: "B",
         eliminatoria: "Fase de grupos",
-        banderaEstadosUnidos:
+        banderaLocal:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png",
-        banderaGales:
+        banderaVisitante:
           "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Flag_of_Wales_%28variant%29.svg/1200px-Flag_of_Wales_%28variant%29.svg.png",
       },
     ],
