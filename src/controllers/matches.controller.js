2516591f0321cdda2fd1bdb4d8360107f4b31f8b
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-17 18:45:14 +0100

    partidos del martes 22/11 añadidos

--- src/controllers/matches.controller.js
+++ src/controllers/matches.controller.js
@@ -51,6 +51,54 @@ controller.matches = (req, res) => {
         banderaVisitante:
           "https://upload.wikimedia.org/wikipedia/en/d/dc/Wales_national_football_team_logo.svg",
       },
+      {
+        id: 5,
+        partido: "Argentina - Arabia Saudita",
+        fecha: "22/11/2022",
+        hora: "11:00",
+        grupo: "C",
+        eliminatoria: "Fase de grupos",
+        banderaLocal:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1200px-Flag_of_Argentina.svg.png",
+        banderaVisitante:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/1200px-Flag_of_Saudi_Arabia.svg.png",
+      },
+      {
+        id: 6,
+        partido: "Dinamarca - Tunez",
+        fecha: "22/11/2022",
+        hora: "14:00",
+        grupo: "D",
+        eliminatoria: "Fase de grupos",
+        banderaLocal:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/1200px-Flag_of_Denmark.svg.png",
+        banderaVisitante:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/1200px-Flag_of_Tunisia.svg.png",
+      },
+      {
+        id: 7,
+        partido: "México - Polonia",
+        fecha: "22/11/2022",
+        hora: "17:00",
+        grupo: "C",
+        eliminatoria: "Fase de grupos",
+        banderaLocal:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1200px-Flag_of_Mexico.svg.png",
+        banderaVisitante:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/12/Flag_of_Poland.svg/1200px-Flag_of_Poland.svg.png",
+      },
+      {
+        id: 8,
+        partido: "Francia - Australia",
+        fecha: "22/11/2022",
+        hora: "20:00",
+        grupo: "D",
+        eliminatoria: "Fase de grupos",
+        banderaLocal:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Flag_of_France.svg/1200px-Flag_of_France.svg.png",
+        banderaVisitante:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1200px-Flag_of_Australia_%28converted%29.svg.png",
+      },
     ],
   });
 };
